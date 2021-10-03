51f97aceeb5676ec7aa76141f52b8966376e1eaf
Soham <sattigeri.soham@gmail.com>
2021-10-03 15:23:06 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,12 +30,13 @@ export default function Example() {
     });
   }
   return (
-    <Disclosure as="nav" className="bg-blue-300">
+    <div className="sticky">
+    <Disclosure as="nav" className="bg-purple-300 ">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="sticky flex items-center justify-between h-16">
+              <div className=" flex relative inset-y-3 left-0  items-center  sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -48,10 +49,10 @@ export default function Example() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <h1 className="text-white text-2xl subpixel-antialiased font-sans">ManhaShanti</h1>
+                  <h1 className="text-white text-6xl subpixel-antialiased" id="Hero">Manha Shanti</h1>
                 </div>
                 <div className="hidden sm:block sm:ml-6 ml-lg">
-                  <div className="flex space-x-4 text-center" >
+                  <div className="flex space-x-4 text-center mt-3 ml-" >
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -158,5 +159,6 @@ export default function Example() {
         </>
       )}
     </Disclosure>
+    </div>
   )
 }
