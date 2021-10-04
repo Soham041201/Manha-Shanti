97e2394240cce001d90f60b933fcf391b9eee821
Vaishanvi <vaishnavi.deshpande20@vit.edu>
2021-10-04 12:45:27 +0530

    added image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div class>
-            
-            <h1 class="font-semibold text-center text-8xl text-white  ml-10 mt-10 p-0.5">ManhaShanti</h1>
+        <div class="flex"> 
+        
+            <div class="w-1/2">
+            <h1 class="font-bold break-normal text-left text-8xl text-white  ml-20 mt-10 p-0.5">Manha<br/>Shanti<hr class="mt-10" id="line"/></h1>
+            </div>
+
+            <div className ="w-1/2">
+             <img class="p-20 rounded-full" src ="./images/img1.jpg" alt=""/> 
+            </div>
  
-            <p class= "text-center text-white ml-10 mt-10 p-1.5">Diary | Podcasts | Music</p>
+            {/* <p class= "text-center text-white ml-10 mt-10 p-1.5">Diary | Podcasts | Music</p> */}
             
         </div>
+        
     );
 }
   
