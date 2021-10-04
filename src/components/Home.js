1abdafccab9feba2f96ed1fda136c1f7d6a498b4
Soham <sattigeri.soham@gmail.com>
2021-10-04 13:17:24 +0530

    text changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
         <div class="flex"> 
         
             <div class="w-1/2">
-            <h1 class="font-bold break-normal text-left text-8xl text-white  ml-20 mt-10 p-0.5">Manha<br/>Shanti<hr class="mt-10" id="line"/></h1>
+             <h1 class="font-bold break-normal text-left text-7xl text-white  ml-20 mt-10 p-0.5">Manha<br/>Shanti<hr class="mt-3" id="line"/></h1>
+            <p class="font-semibold text-white text-left ml-20 p-0.5 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 
             <div className ="w-1/2">
-             <img class="p-20 rounded-full" src ="./images/img1.jpg" alt=""/> 
+             <img class="p-20 rounded-full"id="img" src ="./images/img1.jpg"alt=""/> 
             </div>
- 
+
             {/* <p class= "text-center text-white ml-10 mt-10 p-1.5">Diary | Podcasts | Music</p> */}
             
         </div>
