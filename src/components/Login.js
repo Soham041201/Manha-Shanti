bbca8b16a1701a00a7d32847166a6b83d2539824
Soham <sattigeri.soham@gmail.com>
2021-10-05 11:42:57 +0530

    Login design changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,45 @@
 
-import React,{useRef} from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React,{useState} from "react";
 import {Link,useHistory } from "react-router-dom";
 import { loginFn } from '../Auth'
 export default function SignUp() {
 
     const page= useHistory()
-    const emailRef = useRef()
-    const passwordRef = useRef()
+	const[email,setEmail] = useState("")
+	const[password,setPassword] = useState("")
   const handleLogin =(e)=>{
-        loginFn(emailRef.current.value, passwordRef.current.value,page)
+    	e.preventDefault();
+        loginFn(email, password)
         page.push("/home")
     }
 
 
   return (
-    <div className="login">
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb4-4">Login</h2>
-          <Form>
-            <Form.Group id="email">
-               <div className="error">
-                   </div> 
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" ref={emailRef}required/>
-            </Form.Group>
-            <Form.Group id="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" ref={passwordRef}required/>
-            </Form.Group>
-            
-          </Form>
-          <Button type="button" onClick={handleLogin} className="w-100">Login</Button>  
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Dont have an account?  <Link to="/register">Register</Link>
-      </div>
+    <div className="login mt-5">
+<div class="flex justify-center items-center">
+	<div class="py-7 px-12 bg-white rounded-2xl shadow-xl z-20">
+		<div>
+			<h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Login  into your account</h1>
+			<p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">Login into your account using your registered email and password</p>
+		</div>
+		<div class="space-y-4">
+			<form onSubmit={handleLogin}>
+			<input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email Addres" className="block text-sm py-3 px-4 rounded-lg w-full border focus:outline-none focus:ring focus:border-blue-500" />
+			<input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" className="block text-sm py-3 px-4 rounded-lg w-full border focus:outline-none focus:ring focus:border-blue-500 mt-2" />
+			<button class="py-3 w-64 text-xl mt-3 ml-7 text-white bg-purple-400 rounded-2xl"  href="/home" type="submit">Login</button>
+			</form>
+	</div>
+			<div class="text-center mt-6">
+				
+				<p class="mt-4 text-sm">Dont have an account? <span class="underline cursor-pointer"> <Link to="/register">Register</Link> </span>
+				</p>
+			</div>
+		</div>
+		<div class="w-40 h-40 absolute bg-purple-300 rounded-full top-30 right-12 hidden md:block "></div>
+		<div
+			class="w-20 h-20 absolute bg-purple-300 rounded-full bottom-20 left-10 transform  hidden md:block">
+		</div>
+	</div>
     </div>
   );
 }
--- src/components/Music.js
+++ src/components/Music.js
@@ -4,6 +4,7 @@ export default function Music() {
     return (
         <div>
             <h4>Music Page</h4>
+            <img className="p-20 rounded-lg "id="img" src ="https://image.freepik.com/free-vector/listening-happy-music-concept-illustration_114360-5094.jpg"alt=""/> 
         </div>
     )
 }
