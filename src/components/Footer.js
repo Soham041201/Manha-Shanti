c3a28ec8ed76ba51f8b08ddaa751cb5c19a98557
Soham <sattigeri.soham@gmail.com>
2021-10-06 22:57:45 +0530

    copyright added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 export default function Footer() {
+    const year = new Date()
     return (
        
             <footer className="footer">
-            <p class= "bg-green-500 text-center text-white  mt-10 p-10 "> <Link to="/diary">Diary</Link> | <Link to="/podcast">Podcasts</Link> | <Link to="/music">Music</Link></p>
+            <p className= "bg-green-500 text-center text-white pt-10  "> <Link to="/diary">Diary</Link> | <Link to="/podcast">Podcasts</Link> | <Link to="/music">Music</Link></p>
+            <p className= "bg-green-500 text-center text-white  pb-3 pt-5 ">© Soham Sattigeri and Vaishnavi Deshpande {year.getFullYear()}</p>
             </footer>
     
     )
