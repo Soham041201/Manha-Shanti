72740e1157bd6c97d0727d3a049b6777548668ea
Soham <sattigeri.soham@gmail.com>
2021-10-20 21:04:08 +0530

    diary

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -7,9 +7,10 @@ export default function DiaryList({ blogs }) {
   
   return (
     <div className="rounded-xl bg-white ml-10 p-2 h-screen w-48">
-      <h1 className="text-2xl">Diary List</h1>
+      <h1 className="text-2xl mb-10 mt-3">Diary List</h1>
+      <hr/>
       {blogs?blogs.map((route, index) => (
-        <div key={route.id} className="p-2 bg-red-100 m-3 rounded-xl">
+        <div key={route.id} className="p-2 bg-red-100 m-3 rounded-l">
           <Link to={`/diary/${index}`}>
             <h1>{route.date}</h1>
             <h3>{route.title}</h3>
