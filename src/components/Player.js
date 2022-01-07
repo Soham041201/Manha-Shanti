5b7a3b55ea90d8ad093b16111ff5ea8bda23a8df
Soham Sattigeri <79072777+Soham041201@users.noreply.github.com>
2022-01-07 09:04:07 +0530

    player bug fixed (#1)

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,108 +1,97 @@
-import React,{useState,useRef, useEffect} from 'react'
-import PauseOutlinedIcon from '@mui/icons-material/PauseOutlined';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box';
-import { getDownloadURL, getStorage, listAll, ref} from "firebase/storage";
-const Player = ()=> {
-  const storage = getStorage();
-  const [songs,setSongs] = useState([]);
-    const audioEl = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentSongIndex, setCurrentSongIndex] = useState(0);
-    
+import PauseOutlinedIcon from "@mui/icons-material/PauseOutlined";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import React, { useEffect, useRef, useState } from "react";
 
+const Player = ({ songs }) => {
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
 
-    const handleList = async () => {
-      const listRef = ref(storage, '/songs');
-      await listAll(listRef)
-      .then((res) => {
-        res.items.forEach((itemRef) => {
-          getDownloadURL(ref(storage,itemRef._location.path_ )).then((url) => {
-              setSongs([...songs,url]);
-            });
-        }); 
-      }).catch((error) => {
-        console.log(error);
-      });
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
     }
-    
-    useEffect(()=>{
-      handleList()
-        if(isPlaying){
-            audioEl.current.play();
-        }else{
-            audioEl.current.pause();
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[isPlaying]);
+  }, [isPlaying, currentSongIndex]);
 
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      if (currentSongIndex === songs.length - 1) {
+        setCurrentSongIndex(0);
+      } else {
+        setCurrentSongIndex(currentSongIndex + 1);
+      }
+    } else {
+      if (currentSongIndex === 0) {
+        setCurrentSongIndex(songs.length - 1);
+      } else {
+        setCurrentSongIndex(currentSongIndex - 1);
+      }
+    }
+  };
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        width: "100%",
+        bottom: 64,
+        px: 2,
+        background: "rgba(20, 20, 20, 0.4)",
+        height: 97,
+        backdropFilter: "blur(4px)",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <audio src={songs[currentSongIndex]?.url} ref={audioEl}></audio>
+      <Box
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          m: 2,
+          p: 1,
+        }}
+      >
+        <Button onClick={() => SkipSong(false)}>
+          <SkipPreviousIcon sx={{ color: "white" }} />
+        </Button>
 
-    const SkipSong = (forwards = true) => {
-      if(forwards){
-        setCurrentSongIndex(()=>{
-            let temp = currentSongIndex;
-            temp++;
-            if(temp> songs.length -1){
-                temp =0;
-            }
-
-            return temp;
-        })
-    }else{
-        setCurrentSongIndex(()=>{
-            let temp = currentSongIndex;
-            temp--;
-
-            if(temp<0 ){
-                temp =songs.length -1;
-            }
-
-            return temp;
-        });
-    }}
-    return (
-       <Box >
-           <audio src={songs[currentSongIndex]} ref={audioEl}></audio>
-           <Box sx={{
-             justifyContent:'center',
-             alignItems:'center',
-              display:'flex',
-              m:0,
-              p:1,
-              backgroundColor:'gray',
-           }}>
-           <Button onClick={() => SkipSong(false)}>
-            <SkipPreviousIcon sx={{ color: 'white' }} />
-          </Button>
-      
-                <Button
-            variant='outlined'
+        {songs[currentSongIndex] ? (
+          <Button
+            variant="outlined"
             sx={{
-              borderColor: 'white',
+              borderColor: "white",
               mx: 2,
               p: 1,
               height: 60,
-              borderRadius: '100%',
+              borderRadius: "100%",
             }}
             onClick={() => {
               setIsPlaying(!isPlaying);
             }}
           >
             {!isPlaying ? (
-              <PlayArrowIcon sx={{ color: 'white' }} />
+              <PlayArrowIcon sx={{ color: "white" }} />
             ) : (
-              <PauseOutlinedIcon sx={{ color: 'white' }} />
+              <PauseOutlinedIcon sx={{ color: "white" }} />
             )}
           </Button>
-          <Button onClick={() => SkipSong()}>
-            <SkipNextIcon sx={{ color: 'white' }} />
-          </Button>
-           </Box>
-       </Box>
-    )
-}
+        ) : (
+          <CircularProgress />
+        )}
+        <Button onClick={() => SkipSong()}>
+          <SkipNextIcon sx={{ color: "white" }} />
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
 export default Player;
